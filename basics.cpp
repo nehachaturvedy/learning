7d36f687bcc5e12dabd0b5df971c26e36f645dd2
Neha <nehachaturvedi990@gmail.com>
2023-10-06 15:28:52 +0530

    first commit

---
+++ basics.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cin >> i;
+
+    if (i % 2 == 0)
+    {
+        cout << "i is even";
+    }
+    else
+    {
+        cout << "i is odd";
+    }
+
+    cout << i;
+    return 0;
+}
